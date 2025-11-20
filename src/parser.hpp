c2ad834feccd8df47e8487f275e59acbeefa4f13
Lordink <infernalstudios@gmail.com>
2025-11-20 22:00:35 +0700

    Some code cleanups and refactors

--- src/parser.hpp
+++ src/parser.hpp
@@ -7,9 +7,10 @@
 #include <string>
 #include <variant>
 
-
 // Root expression type
-struct Expr {};
+struct Expr {
+    virtual ~Expr() = default;
+};
 
 using ExprPtr = std::unique_ptr<Expr>;
 
@@ -18,10 +19,13 @@ struct Expr_Literal : public Expr {
         double value = 0.0;
         explicit Number(const double value) : value(value) {}
     };
-    struct String { std::string value; };
-    struct True{};
-    struct False{};
-    struct Nil{};
+    struct String {
+        std::string value;
+        explicit String(std::string value) : value(std::move(value)) {}
+    };
+    struct True {};
+    struct False {};
+    struct Nil {};
 
     using LiteralVariant = std::variant<Number, String, True, False, Nil>;
     LiteralVariant inner;
@@ -50,7 +54,8 @@ struct Expr_Unary : public Expr {
     EUnaryOperator op;
     ExprPtr inner;
 
-    explicit Expr_Unary(const EUnaryOperator op, ExprPtr inner) : op(op), inner(std::move(inner)) {}
+    explicit Expr_Unary(const EUnaryOperator op, ExprPtr inner)
+        : op(op), inner(std::move(inner)) {}
 };
 
 struct Expr_Binary : public Expr {
@@ -81,7 +86,8 @@ struct Expr_Binary : public Expr {
     EBinaryOperator op;
     ExprPtr right;
 
-    explicit Expr_Binary(ExprPtr left, const EBinaryOperator op, ExprPtr right) : left(std::move(left)), op(op), right(std::move(right)) {}
+    explicit Expr_Binary(ExprPtr left, const EBinaryOperator op, ExprPtr right)
+        : left(std::move(left)), op(op), right(std::move(right)) {}
 };
 
 // Just create a mocked expression as if we already parsed something
@@ -90,14 +96,11 @@ struct Expr_Binary : public Expr {
 inline ExprPtr mock_parsed() {
     using std::make_unique;
 
-    auto expr =  make_unique<Expr_Binary>(
-        make_unique<Expr_Unary>(
-            Expr_Unary::EUnaryOperator::Minus,
-            make_unique<Expr_Literal>(123.0)
-        ),
+    auto expr = make_unique<Expr_Binary>(
+        make_unique<Expr_Unary>(Expr_Unary::EUnaryOperator::Minus,
+                                make_unique<Expr_Literal>(123.0)),
         Expr_Binary::EBinaryOperator::Mul,
-        make_unique<Expr_Grouping>(make_unique<Expr_Literal>(45.67))
-    );
+        make_unique<Expr_Grouping>(make_unique<Expr_Literal>(45.67)));
 
     return expr;
 }
