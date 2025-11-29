e0aec1abe4a58325516d2ca636cf575f5558c7b9
Lordink <infernalstudios@gmail.com>
2025-11-29 17:44:49 +0700

    Pretty-printing visitor up & running

--- src/parser.hpp
+++ src/parser.hpp
@@ -4,9 +4,9 @@
  **/
 
 #include <memory>
+#include <print>
 #include <string>
 #include <variant>
-#include <print>
 
 struct Expr_Grouping;
 struct Expr_Literal;
@@ -14,7 +14,7 @@ struct Expr_Unary;
 struct Expr_Binary;
 
 class Visitor {
-public:
+  public:
     virtual void visit_literal(Expr_Literal const& literal) const = 0;
     virtual void visit_grouping(Expr_Grouping const& grouping) const = 0;
     virtual void visit_unary(Expr_Unary const& unary) const = 0;
@@ -127,40 +127,78 @@ struct Expr_Binary : public Expr {
 
 // TODO make this output string (template class?) instead?
 class Visitor_PPrint : public Visitor {
-public:
+  public:
     virtual void visit_unary(Expr_Unary const& unary) const override {
-        const char op = unary.op == Expr_Unary::EUnaryOperator::Minus
-            ? '-'
-            : '!';
+        const char op =
+            unary.op == Expr_Unary::EUnaryOperator::Minus ? '-' : '!';
         std::print("{}", op);
         unary.inner->accept(*this);
     }
 
     virtual void visit_literal(Expr_Literal const& literal) const override {
-        std::visit([](auto&& var){
-            using T = std::decay_t<decltype(var)>;
-            using std::is_same_v;
-            if constexpr (is_same_v<T, Expr_Literal::Number>) {
-                std::print("{}", var.value);
-            } else if constexpr (is_same_v<T, Expr_Literal::String>) {
-                std::print("{}", var.value);
-            } else if constexpr (is_same_v<T, Expr_Literal::True>) {
-                std::print("true");
-            } else if constexpr (is_same_v<T, Expr_Literal::False>) {
-                std::print("false");
-            } else if constexpr (is_same_v<T, Expr_Literal::Nil>) {
-                std::print("nil");
-            } else {
-                std::unreachable();
-            }
-        }, literal.inner);
+        std::visit(
+            [](auto&& var) {
+                using T = std::decay_t<decltype(var)>;
+                using std::is_same_v;
+                if constexpr (is_same_v<T, Expr_Literal::Number>) {
+                    std::print("{}", var.value);
+                } else if constexpr (is_same_v<T, Expr_Literal::String>) {
+                    std::print("{}", var.value);
+                } else if constexpr (is_same_v<T, Expr_Literal::True>) {
+                    std::print("true");
+                } else if constexpr (is_same_v<T, Expr_Literal::False>) {
+                    std::print("false");
+                } else if constexpr (is_same_v<T, Expr_Literal::Nil>) {
+                    std::print("nil");
+                } else {
+                    std::unreachable();
+                }
+            },
+            literal.inner);
     }
 
     virtual void visit_binary(Expr_Binary const& binary) const override {
-        throw std::logic_error("TODO implement");
+        binary.left->accept(*this);
+        std::string op;
+        switch (binary.op) {
+        case Expr_Binary::EBinaryOperator::EqEq:
+            op = "==";
+            break;
+        case Expr_Binary::EBinaryOperator::NotEq:
+            op = "!=";
+            break;
+        case Expr_Binary::EBinaryOperator::Less:
+            op = "<";
+            break;
+        case Expr_Binary::EBinaryOperator::LessOrEq:
+            op = "<=";
+            break;
+        case Expr_Binary::EBinaryOperator::Greater:
+            op = ">";
+            break;
+        case Expr_Binary::EBinaryOperator::GreaterOrEq:
+            op = ">=";
+            break;
+        case Expr_Binary::EBinaryOperator::Plus:
+            op = "+";
+            break;
+        case Expr_Binary::EBinaryOperator::Minus:
+            op = "-";
+            break;
+        case Expr_Binary::EBinaryOperator::Mul:
+            op = "*";
+            break;
+        case Expr_Binary::EBinaryOperator::Div:
+            op = "/";
+            break;
+        }
+        std::print(" {} ", op);
+        binary.right->accept(*this);
     }
     virtual void visit_grouping(Expr_Grouping const& grouping) const override {
-        throw std::logic_error("TODO implement");
+        std::print("( ");
+        grouping.inner->accept(*this);
+        std::print(" )");
     }
 };
 
@@ -170,14 +208,14 @@ public:
 inline ExprPtr mock_parsed() {
     using std::make_unique;
 
-    // auto expr = make_unique<Expr_Binary>(
-    //     make_unique<Expr_Unary>(Expr_Unary::EUnaryOperator::Minus,
-    //                             make_unique<Expr_Literal>(123.0)),
-    //     Expr_Binary::EBinaryOperator::Mul,
-    //     make_unique<Expr_Grouping>(make_unique<Expr_Literal>(45.67)));
+    auto expr = make_unique<Expr_Binary>(
+        make_unique<Expr_Unary>(Expr_Unary::EUnaryOperator::Minus,
+                                make_unique<Expr_Literal>(123.0)),
+        Expr_Binary::EBinaryOperator::Mul,
+        make_unique<Expr_Grouping>(make_unique<Expr_Literal>(45.67)));
 
-    auto expr = make_unique<Expr_Unary>(Expr_Unary::EUnaryOperator::Minus,
-        make_unique<Expr_Literal>(500.19));
+    // auto expr = make_unique<Expr_Unary>(Expr_Unary::EUnaryOperator::Minus,
+    //     make_unique<Expr_Literal>(500.19));
 
     return expr;
 }
