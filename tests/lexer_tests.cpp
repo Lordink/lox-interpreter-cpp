f3f311934f6bb1037a417afa15a295329daaf36a
Lordink <infernalstudios@gmail.com>
2025-12-13 14:12:14 +0700

    Some more lexer tests

--- tests/lexer_tests.cpp
+++ tests/lexer_tests.cpp
@@ -16,3 +16,55 @@ TEST_CASE("Lexing empty string", "[lexer]") {
     REQUIRE(std::holds_alternative<EndOfFile>(value));
 
 }
+
+TEST_CASE("Lexing a string literal", "[lexer]") {
+    const std::string in = "\"some string\"";
+    size_t num_errs = 0;
+    const auto out = lex(in, num_errs);
+    CHECK( num_errs == 0 );
+    REQUIRE( out.size() == 2 );
+
+    const auto& elem_1 = out[0];
+    const auto& elem_2 = out[1];
+    REQUIRE(elem_1.has_value());
+    REQUIRE(elem_2.has_value());
+
+    const auto elem_1_val = elem_1.value();
+    REQUIRE(std::holds_alternative<StringLiteral>(elem_1_val));
+    const auto literal = std::get<StringLiteral>(elem_1_val);
+    REQUIRE(literal.literal == "some string");
+
+    REQUIRE(std::holds_alternative<EndOfFile>(elem_2.value()));
+}
+
+TEST_CASE("/() division and parens", "[lexer]") {
+    const std::string in = "/()";
+    size_t num_errs = 0;
+    const auto out = lex(in, num_errs);
+
+    CHECK( num_errs == 0 );
+    REQUIRE( out.size() == 4 );
+
+    REQUIRE(std::holds_alternative<Slash>(out[0].value()));
+    REQUIRE(std::holds_alternative<LeftParen>(out[1].value()));
+    REQUIRE(std::holds_alternative<RightParen>(out[2].value()));
+
+    REQUIRE(std::holds_alternative<EndOfFile>(out[3].value()));
+}
+
+TEST_CASE("={===} assignment and figure brackets", "[lexer]") {
+    const std::string in = "={===}";
+    size_t num_errs = 0;
+    const auto out = lex(in, num_errs);
+
+    CHECK( num_errs == 0 );
+    REQUIRE( out.size() == 6 );
+
+    REQUIRE(std::holds_alternative<Assign>(out[0].value()));
+    REQUIRE(std::holds_alternative<LeftBrace>(out[1].value()));
+    REQUIRE(std::holds_alternative<Equals>(out[2].value()));
+    REQUIRE(std::holds_alternative<Assign>(out[3].value()));
+    REQUIRE(std::holds_alternative<RightBrace>(out[4].value()));
+
+    REQUIRE(std::holds_alternative<EndOfFile>(out[5].value()));
+}
